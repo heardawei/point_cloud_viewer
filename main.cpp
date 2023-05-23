8c957a12fa4d9150f116dd0743b46ddcc09dd003
李大伟 <ldw01441929@alibaba-inc.com>
2023-05-23 20:06:55 +0800

    编译通过

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <QApplication>
+#include <QMainWindow>
+
+#include "pclviewer.h"
+
+int main (int argc, char *argv[])
+{
+  QApplication a (argc, argv);
+  PCLViewer w;
+  w.show ();
+  return a.exec ();
+}
